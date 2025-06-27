e3a821c070e199ac3a17b71c88e062ef89d45727
shaitansix <shaitansix3@gmail.com>
2025-06-26 22:23:14 -0500

    Configuracion de cors

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5173', 
+    '*'
 ]
 
 # Application definition
