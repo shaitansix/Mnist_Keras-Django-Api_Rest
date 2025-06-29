66488af1402b84f3f27d21b574d615d457d32e4a
shaitansix <shaitansix3@gmail.com>
2025-06-29 16:51:57 -0500

    Despliegue en render

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pyenv install $PYTHON_VERSION -s
+pyenv global $PYTHON_VERSION
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
